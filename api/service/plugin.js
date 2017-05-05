e2ce4c7bda35ec615f4254ffa9ebcb3e0d817e3b
Momonta <mr.guermazi.mohamed@gmail.com>
2017-05-05 16:11:48 +0200

    list uploaders momo

--- api/service/plugin.js
+++ api/service/plugin.js
@@ -420,4 +420,32 @@ module.exports = function (options) {
         });
     });
 
+/**
+     * get the list of the uploaders
+     * @function
+     * @param {JSON object} msg - no information
+     * @param {function} respond - response after operation : respond(err, JSON object response);
+     */
+    this.add('role:uploaders,cmd:list', (msg, respond) => {
+        let validation = new Promise((resolve, reject) => {
+            resolve();
+        });
+		let multi = clientRedis.multi();
+		let upsv=[];
+		validation.then(() => clientRedis.scanAsync('0','MATCH','uploader:?','count','100000')) // ? to be modified if there are >= 10 ups
+	    .then((uploaders) => {
+			upsv = uploaders[1];
+		})
+		.then((upsv) => {
+				return new Promise( (resolve, reject) => {respond(null, { 'uploaders':upsv,'code': 200 , 'status': "uploaders listed succesfully." }); resolve();}, null );
+			});
+		})
+
+        //.then(() => {return new Promise( (resolve, reject) => {respond(null, { 'code': 200 , 'status': "Servers load counted succesfully." }); resolve();}, null );} )
+        .catch(err => {
+            respond(`Error on listing uploaders : ${err}`, { 'code': 500 , 'status': null });
+        });
+    });
+
+
 };
